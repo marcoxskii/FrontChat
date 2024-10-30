9e3cae332cc6f66d582eb05fdbca8b378fedfcbd
Marco Cajamarca <marcoxc01@gmail.com>
2024-10-29 20:37:22 -0500

    User, Msg & Date added

--- src/app/Components/chat/chat.component.ts
+++ src/app/Components/chat/chat.component.ts
@@ -1,12 +1,35 @@
+import { NgFor, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule, DatePipe],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+  messages: { text: string, timestamp: Date }[] = [];
+  userName: string = '';
+  hasEnteredName: boolean = false;
 
+  sendMessage() {
+    const inputElement = document.getElementById('messageInput') as HTMLInputElement;
+    const message = inputElement.value;
+    if (!this.hasEnteredName) {
+      if (message.trim()) {
+        this.userName = message.trim();
+        this.hasEnteredName = true;
+        inputElement.value = '';
+      } else {
+        alert('Por favor, ingresa tu nombre.');
+      }
+    } else {
+      if (message.trim()) {
+        this.messages.push({ text: `${this.userName}: ${message}`, timestamp: new Date() });
+        inputElement.value = '';
+      }
+    }
+  }
 }
